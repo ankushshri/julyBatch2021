5557f4df4e32baf506022384bfec20694142ccc3
shri <shri@192.168.0.103>
2021-12-04 15:53:53 +0530

    Add Screenshot method in utils

--- UcnApp/src/main/java/utilitypackage/Utiles.java
+++ UcnApp/src/main/java/utilitypackage/Utiles.java
@@ -1,5 +1,24 @@
 package utilitypackage;
 
-public class Utiles {
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class Utiles {
+ public static void screenShot(WebDriver driver) throws IOException {
+	 DateTimeFormatter d=  DateTimeFormatter.ofPattern("yyyy-MM-dd a");
+	 LocalDateTime now = LocalDateTime.now();
+	 
+	 
+ File scr=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+ File dest=new File("F:\\velocity\\AUTOMATION\\SELENIUM TOPICS\\Screenshot\\Proofss\\Test_"+d.format(now)+".jpg");
+ FileHandler.copy(scr, dest);
+ }
+	
 }
